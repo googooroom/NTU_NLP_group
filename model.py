84bda42c1a573697c109d3bc1772374b643d22f8
rgan19 <rachel_gan@mymail.sutd.edu.sg>
2023-04-07 00:47:44 +0800

    modified softprompt to allow tuning for other models

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from transformers import RobertaForSequenceClassification, RobertaConfig
 
 class SoftPromptTuning(nn.Module):
-    def __init__(self, model: RobertaForSequenceClassification, prompt_length: int):
+    def __init__(self, model, prompt_length: int):
         super(SoftPromptTuning, self).__init__()
         self.model = model
         self.prompt_length = prompt_length
@@ -15,16 +15,17 @@ class SoftPromptTuning(nn.Module):
 
     def forward(self, input_ids, attention_mask=None, labels=None):
         # Prepend the soft prompt embeddings to the input embeddings
-        input_embeddings = self.model.roberta.get_input_embeddings()(input_ids)
+        # input_embeddings = self.model.roberta.get_input_embeddings()(input_ids)
+        input_embeddings = self.model.get_input_embeddings()(input_ids) # this would work too, 
         prompt_embeddings = self.soft_prompt_embeddings.unsqueeze(0).repeat(input_embeddings.size(0), 1, 1)
         combined_embeddings = torch.cat((prompt_embeddings, input_embeddings), dim=1)
-
+        
         # Adjust the attention mask if provided
         if attention_mask is not None:
             attention_mask = torch.cat((torch.ones(input_embeddings.size(0), self.prompt_length).to(attention_mask.device), attention_mask), dim=1)
 
         # Feed the combined embeddings through the model
-        outputs = self.model(inputs_embeds=combined_embeddings, attention_mask=attention_mask, labels=labels)
+        outputs = self.model(inputs_embeds=combined_embeddings, attention_mask=attention_mask, labels=labels) # GPT2 log error appears when this runs
 
         return outputs
     
