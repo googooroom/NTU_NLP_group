801fb4565299a8875d5c64a4f4d8cb7ea79db80c
jojopuppet <weisheng9822@gmail.com>
2023-04-06 03:24:18 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,11 +6,12 @@ train_file="/notebooks/nlp/data/train.csv"
 instruction_file="/notebooks/nlp/data/instructions.txt"
 max_len=64
 batch_size=64
-lr=1e-5
+lr=1e-2
 epochs=10
 weight_decay=1e-4
 test_file="/notebooks/nlp/data/test.csv"
 prompt_length=20
+apply_soft_prompt=True
 
 # run the main script
 python main.py \
@@ -21,3 +22,4 @@ python main.py \
     --lr $lr \
     --weight_decay $weight_decay \
     --prompt_length $prompt_length \
+    --apply_soft_prompt $apply_soft_prompt \
